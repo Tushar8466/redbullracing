6eefb0882fdcc6acc525db0937ffca958fbc4bbc
tusharsingla22222-source <tusharsingla22222@gmail.com>
2026-02-06 17:43:06 +0530

    Made a herosection using spline

---
+++ src/components/ui/RedBullCar.tsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function RedBullCar() {
+  return (
+    <main className="h-screen w-screen overflow-hidden">
+      <Spline
+        scene="https://prod.spline.design/wuV5cpTE47ohcMKk/scene.splinecode"
+      />
+    </main>
+  );
+}
